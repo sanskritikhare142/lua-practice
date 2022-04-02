099a4fb9db62168936aad399e0ab83720f6e3939
Sanskriti Khare <60572554+sanskritikhare142@users.noreply.github.com>
2022-04-03 00:35:07 +0530

    ✨  Print strings and add comments

---
+++ hello.lua
@@ -0,0 +1,18 @@
+print("Hello World")
+--Comments
+--[[multi-line
+comments
+]]
+
+name="Sanskriti"
+io.write("Printing the size of name: ", #name, "\n")
+io.write("Printing the value of \'name\': ", name, "\n")
+--[[
+Printing some special characters inside the quotes:
+    \n  New line
+    \b  Backspace
+    \t  Tab
+    \\  Back slash
+    \"  Double Quote
+    \'  Single Quotes
+]]
